64aa014be44f8af1abc4ee563676410933c33c84
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-08 21:54:59 -0600

    Conectando nuestros servicios como clientes Eureka

--- src/main/java/com/aboylan/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/aboylan/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,10 +2,8 @@ package com.aboylan.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
