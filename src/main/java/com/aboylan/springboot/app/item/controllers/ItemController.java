35a6f68cd05f9fc1945e45424ddb77a4e43b740f
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-28 19:39:06 -0600

    Implementando handler CRUD en controlador de servicio items

--- src/main/java/com/aboylan/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/aboylan/springboot/app/item/controllers/ItemController.java
@@ -15,10 +15,15 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aboylan.springboot.app.item.models.Item;
@@ -33,7 +38,7 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 public class ItemController {
 
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
-	
+
 	@Autowired
 	private Environment env;
 
@@ -43,7 +48,7 @@ public class ItemController {
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
-	
+
 	@Value("${configuracion.texto}")
 	private String texto;
 
@@ -73,7 +78,7 @@ public class ItemController {
 	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
 	}
-	
+
 	public Item metodoAlternativo(@PathVariable Long id, @PathVariable Integer cantidad, Throwable e) {
 
 		logger.info(e.getMessage());
@@ -89,8 +94,9 @@ public class ItemController {
 
 		return item;
 	}
-	
-	public CompletableFuture<Item> metodoAlternativo2(@PathVariable Long id, @PathVariable Integer cantidad, Throwable e) {
+
+	public CompletableFuture<Item> metodoAlternativo2(@PathVariable Long id, @PathVariable Integer cantidad,
+			Throwable e) {
 
 		logger.info(e.getMessage());
 
@@ -105,22 +111,40 @@ public class ItemController {
 
 		return CompletableFuture.supplyAsync(() -> item);
 	}
-	
+
 	@GetMapping("/obtener-config")
 	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
-		
+
 		logger.info(texto);
-		
+
 		Map<String, String> json = new HashMap<>();
 		json.put("texto", texto);
 		json.put("puerto", puerto);
-		
+
 		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
 			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
 			json.put("autor.email", env.getProperty("configuracion.autor.email"));
-		} 
+		}
 
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		return itemService.update(producto, id);
+	}
+
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		itemService.delete(id);
 	}
 
 }
